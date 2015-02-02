3b09c1d07ec6dba221c219001fc9d5c3a69deec4
ikriv <ivan@home>
2015-02-01 20:02:30 -0500

    Refatored AccountListView

--- src/AquaExplorer/ViewModels/AccountListViewModel.cs
+++ src/AquaExplorer/ViewModels/AccountListViewModel.cs
@@ -1,29 +1,21 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using AquaExplorer.BusinessObjects;
 using AquaExplorer.Services;
-using IKriv.Windows.Mvvm;
 
 namespace AquaExplorer.ViewModels
 {
-    class AccountListViewModel : ViewModelBase, INavigationTarget
+    class AccountListViewModel : ListViewModelBase<Account>
     {
         private readonly INavigationController _controller;
+        private readonly IConfigurationService _configService;
 
         public AccountListViewModel(IConfigurationService configService, INavigationController controller)
         {
             _controller = controller;
-
-            var config = configService.ReadConfiguration(); // make async!
-
-            Accounts = 
-                new ObservableCollection<Account>(
-                    config.Accounts
-                        .Select(c=>new Account { Credentials = c })
-                        .OrderBy(a=>a.Credentials.Account));
+            _configService = configService;
         }
-        public ObservableCollection<Account> Accounts {  get; private set; }
 
         public Account SelectedAccount
         {
@@ -32,28 +24,26 @@ namespace AquaExplorer.ViewModels
         }
         private Account _selectedAccount;
 
-        public void BeginLoad()
-        {
-            if (LoadCompleted != null) LoadCompleted(this);
-        }
-
-        public void BeginStopLoading()
-        {
-            if (LoadCompleted != null) LoadCompleted(this);
-        }
-
         public void OnDoubleCLick()
         {
             NavigateToSelectedAccount();
         }
-
-        public event Action<INavigationTarget> LoadCompleted;
 
         private void NavigateToSelectedAccount()
         {
             if (SelectedAccount == null) return;
             var location = new AzureLocation(SelectedAccount, null);
             _controller.NavigateTo(location);
+        }
+
+        protected override IEnumerable<Account> Load(CancellationToken token)
+        {
+            var config = _configService.ReadConfiguration(); // make async!
+
+            return config.Accounts
+                .Select(c => new Account {Credentials = c})
+                .OrderBy(a => a.Credentials.Account)
+                .ToArray();
         }
     }
 }
